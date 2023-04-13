9a145dbc5bd0dbd110af25aeebf1367b8a1921d2
Joao Matheus <joao.rocha@corebiz.ag>
2023-04-13 20:29:42 -0300

    Configuring views to segmented controller and testing if changing views.

--- Card-Validator/SegmentedViewController.swift
+++ Card-Validator/SegmentedViewController.swift
@@ -9,28 +9,60 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let views = ["Item 0", "Item 1"]
+    private var views: [UIView]!
 
     lazy var segmentedControl: UISegmentedControl = {
-        let segmentedControl = UISegmentedControl(items: views)
+        let segmentedControl = UISegmentedControl(items: ["Endereço", "Cartão de Crédito"])
         segmentedControl.addTarget(self, action: #selector(printItem(_:)), for: .valueChanged)
         segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
         return segmentedControl
     }()
-    
+
+    lazy var viewContainer: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
         setupConstraint()
+        setupViews()
     }
     
+    private func setupViews() {
+        views = [UIView]()
+        [AddressViewController().view,
+         CreditCardViewController().view].forEach { view in
+            views.append(view)
+        }
+        views.forEach { view in
+            viewContainer.addSubview(view)
+        }
+
+        viewContainer.bringSubviewToFront(views[0])
+    }
+
     private func setupConstraint() {
         view.addSubview(segmentedControl)
+        view.addSubview(viewContainer)
         
-        segmentedControl.center = view.center
+        NSLayoutConstraint.activate([
+            segmentedControl.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+
+            viewContainer.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 8),
+            viewContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            viewContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            viewContainer.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
     
     @objc func printItem(_ segmentedControl: UISegmentedControl) {
-        print(views[segmentedControl.selectedSegmentIndex])
+        viewContainer.bringSubviewToFront(views[segmentedControl.selectedSegmentIndex])
     }
+
 }
 
